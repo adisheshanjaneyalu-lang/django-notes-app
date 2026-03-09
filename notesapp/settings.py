c9f82e84b43c7cef45a49be583f5bde9f05e0f13
Adishesh <adishesh.anjaneyalu@gmail.com>
2026-03-09 16:46:46 +0530

    Use environment variables for database settings

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -87,13 +87,14 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
+
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'your_db_name',
-        'USER': 'your_user',
-        'PASSWORD': 'your_password',
-        'HOST': None,  # <--- THIS IS THE PROBLEM
-        'PORT': '3306',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
