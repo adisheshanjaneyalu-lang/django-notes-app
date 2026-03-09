af49b11b134f990d039d6b6e1c88b4b813aac205
Adishesh <adishesh.anjaneyalu@gmail.com>
2026-03-09 17:20:01 +0530

    Change database configuration in settings.py
    Updated database settings to use hardcoded values.

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -87,14 +87,13 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
-
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'NAME': 'notes_db',
+        'USER': 'rootlocalhost',
+        'PASSWORD': 'your_password',
+        'HOST': '172.31.8.194', # Use the Private IP of your Ubuntu server
+        'PORT': '3306',
     }
 }
 
